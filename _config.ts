c0b0747d14a39e941c2dbf593ea2080f1cf2cd5a
pharzan <farzan.tinati@gmail.com>
2025-10-04 12:19:49 +0200

    color code

--- _config.ts
+++ _config.ts
@@ -4,6 +4,7 @@ import sitemap from "lume/plugins/sitemap.ts";
 import feed from "lume/plugins/feed.ts";
 import search from "lume/plugins/search.ts";
 import date from "lume/plugins/date.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
 
 const site = lume({
     location: new URL("https://pharzan.com"),
@@ -15,4 +16,5 @@ site.use(metas());
 site.use(sitemap());
 site.use(search());
 site.use(date());
+site.use(codeHighlight());
 export default site;
